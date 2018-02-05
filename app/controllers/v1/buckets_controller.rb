c380b77f6e4961993910b48af90b2e9628f3015b
DiSalvio <stephenjamesdisalvio@gmail.com>
2018-02-04 21:16:00 -0500

    Add api versioning

---
+++ app/controllers/v1/buckets_controller.rb
@@ -0,0 +1,40 @@
+module V1
+  class BucketsController < ApplicationController
+    before_action :set_bucket, only: [:show, :update, :destroy]
+
+    def index
+      @buckets = current_user.buckets
+      json_response(@buckets)
+    end
+
+    def create
+      @bucket = current_user.buckets.create!(bucket_params)
+      json_response(@bucket, :created)
+    end
+
+    def show
+      json_response(@bucket)
+    end
+
+    def update
+      @bucket.update(bucket_params)
+      head :no_content
+    end
+
+    def destroy
+      @bucket.destroy
+      head :no_content
+    end
+    
+    private
+
+    def bucket_params
+      # whitelist params
+      params.permit(:title, :description, :started_on, :complete_by)
+    end
+
+    def set_bucket
+      @bucket = Bucket.find(params[:id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :buckets do
-    resources :tasks
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :buckets do
+      resources :tasks
+    end
   end
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
